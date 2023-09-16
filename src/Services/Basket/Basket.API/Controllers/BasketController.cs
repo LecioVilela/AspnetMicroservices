380bfe28f04cfdde1eeb83af6c5adca7f75d3be8
LecioVilela <lecio.vilela@yahoo.com>
2023-09-16 17:52:57 -0300

    chore: creating basket controller and configuring services

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,53 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet("{userName}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasketAsync(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            await _basketRepository.UpdateBasketAsync(basket);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{userName}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasketAsync(userName);
+
+            return NoContent();
+        }
+    }
+}
